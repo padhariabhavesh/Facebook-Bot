c50f33049a48630487f342d990a6ef1dd509132e
Bhavesh Padharia <padharia.bhavesh06@gmail.com>
2020-06-08 22:04:08 +0530

    fb-feed-bot

--- fb-feed-bot.py
+++ fb-feed-bot.py
@@ -6,7 +6,7 @@ import facebook
 
 #Token generated from GraphAPI
 
-token = 'EAAnXnU7Ylt4BALVHRPYrqxg9wZBVpYeKZAI1911WFTQn45CAcbuueWydsZAMdZAhXCa4enpwzbcjsUkxmlVxs0ZAPuVHa04P7Uivn1b0WbZCz6OfmgfHAvzFXll52Q6qLJNDFz5LeIFH7LpMQAZBciC5zIS9fuY9oZAI9hYCZBH4ikWKRHBuB72k9SjXYWI1etkRaWb3n2AwDqQZDZD'
+token = 'Enter your access token '
 fb = facebook.GraphAPI(access_token=token)
 
-fb.put_object(parent_object='me', connection_name='feed', message='Hello this is my fourth test for fb page')
+fb.put_object(parent_object='me', connection_name='feed', message='Hello this is test post on feed ')
